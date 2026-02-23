d824af49907858131c50c48db54a8a3a8e8ad6d4
Goonjan Saha <saha.goonjan@gmail.com>
2026-02-23 16:59:15 +0530

    Bug Fixes

--- src/keyarrange/analysis/beat_tracker.py
+++ src/keyarrange/analysis/beat_tracker.py
@@ -12,7 +12,7 @@ def get_beat_times(audio_path: str) -> tuple[np.ndarray, float]:
 
     y, sr = librosa.load(audio_path, mono=True)
 
-    beat_times, bpm = librosa.beat.beat_track(y=y, sr=sr, units='time')
+    bpm, beat_times = librosa.beat.beat_track(y=y, sr=sr, units='time')
 
     if beat_times.size == 0:
         raise ValueError("No beats detected in the audio file.")
@@ -22,5 +22,5 @@ def get_beat_times(audio_path: str) -> tuple[np.ndarray, float]:
 
 if __name__ == "__main__":
     res_1, res_2 = get_beat_times(sys.argv[1])
-    print(res_1)
+    print(len(res_1))
     print(res_2)
